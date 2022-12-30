d68cabef63c3b0dabc96d5bc1452ab7a505ff711
Terry Howald <terry.howald@gmail.com>
2022-12-30 01:43:39 -0500

    Added render function to display index in binary.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,46 @@
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::render::{WindowCanvas, TextureCreator};
+use sdl2::video::WindowContext;
+use sdl2::rect::Rect;
+
 use std::time::Duration;
+use std::path::Path;
+
 use rand::Rng;
 
 // NFT Dimensions in pixels
 const EIGHT_BIT: u32 = 256;
 const NFT_WIDTH: u32 = EIGHT_BIT;
 const NFT_HEIGHT: u32 = EIGHT_BIT;
+
+fn render(canvas: &mut WindowCanvas, texture_creator: &TextureCreator<WindowContext>,
+    font: &sdl2::ttf::Font, value: u32, color: sdl2::pixels::Color) -> Result<(), String> {
+
+    // Set background color
+    canvas.set_draw_color(color);
+    canvas.clear();
+
+    // Draw number value
+    let value_text: String = format!("{value:08b}");
+    //let value_text: String = value.to_string();
+    let surface = font
+        .render(&value_text)
+        .blended(Color::RGB(255-color.r, 255-color.g, 255-color.b))
+        .map_err(|e| e.to_string())?;
+
+    let texture = texture_creator
+        .create_texture_from_surface(&surface)
+        .map_err(|e| e.to_string())?;
+
+    let target = Rect::new(10 as i32, 0 as i32, 200 as u32, 100 as u32);
+    canvas.copy(&texture, None, Some(target))?;
+
+    canvas.present();
+
+    Ok(())
+    }
 
 fn main() -> Result<(), String> {
 
@@ -37,12 +70,16 @@ fn main() -> Result<(), String> {
 
     // Initialize a new canvas
     let mut canvas = window.into_canvas().build()
-        .expect("Could not make a canvas");
+        .expect("Failed to initialize canvas");
 
-    // Clear canvas with drawing color (black) and display
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
-    canvas.clear();
-    canvas.present();
+    // Create texture creator for the new canvas
+    let texture_creator = canvas.texture_creator();
+
+    // Prepare fonts
+    let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string())?;
+    let font_path: &Path = Path::new(&"fonts/OpenSans-Bold.ttf");
+    let mut font = ttf_context.load_font(font_path, 128)?;
+    font.set_style(sdl2::ttf::FontStyle::NORMAL);
 
     // Initialize event pump
     let mut event_pump = sdl_context.event_pump()?;
@@ -51,32 +88,32 @@ fn main() -> Result<(), String> {
     let mut rng = rand::thread_rng();
 
     // Loop through NFTs
-    'running: for i in 1..EIGHT_BIT {
-        // Generate random index and retrieve RGB values
-        let index = rng.gen_range(0..8);
-        let (r, g, b) = roygbiv[index];
-        println!("{} {} {} {}", i, r, g, b);
+    'running: loop {
+        for i in 1..EIGHT_BIT {
+            // Generate random index and retrieve RGB values
+            let index = rng.gen_range(0..8);         
+            let (r, g, b) = roygbiv[index];
+            let color = Color::RGB(r, g, b); 
+            let ib_str: String = format!("{i:08b}");
+            println!("{} {} {} {} {}", i, r, g, b, ib_str);
+         
+            render(&mut canvas, &texture_creator, &font, i, color);            
 
-        // Clear canvas with new drawing color
-        canvas.set_draw_color(Color::RGB(r, g, b));
-        canvas.clear();
+            // See if any events are pending
+            for event in event_pump.poll_iter() {
+                match event {
+                    Event::Quit {..} |
+                    Event::KeyDown { 
+                        keycode: Some(Keycode::Escape), 
+                        .. 
+                        } => break 'running,
+                    _ => {}
+                }
+            } 
 
-        // See if any events are pending
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running;
-                },
-                _ => {}
-            }
-        }        
-
-        // Display newly constructed NFT
-        canvas.present();
-
-        // Sleep for 1 second to show NFT
-        ::std::thread::sleep(Duration::from_secs(1));
+            // Sleep for 1 second to show NFT
+            ::std::thread::sleep(Duration::from_secs(1));
+        }
     }
 
     Ok(())
